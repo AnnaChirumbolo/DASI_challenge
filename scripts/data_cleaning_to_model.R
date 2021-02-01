93469c69e839899b18736954029a9fad1680731a
Anna Chirumbolo <anna.chirumbolo@gmail.com>
2021-02-01 15:47:28 +0100

    added analysis for outliers, pozzi 6 and 9 and other feature variables

--- scripts/data_cleaning_to_model.R
+++ scripts/data_cleaning_to_model.R
@@ -275,5 +275,230 @@ ggsave("img/doganella_boxplot.jpg", dpi = 500, width = 10, height = 7)
 
 
 
+### keeping outliers for pozzi 6 and 9 - from most recent years, and there's 100+ according to the boxplot
+
+### checking outliers for feature variables 
+
+# doganella9
+
+str(doganella9)
+
+
+doganella10 <- doganella9 %>% 
+  gather(key = "temp_sensor", value = "temp.C", Temperature_Monteporzio, 
+         Temperature_Velletri) %>%
+  gather(key = "rain_sensor", value = "rain.mm", Rainfall_Monteporzio,
+         Rainfall_Velletri)
+
+
+## vis hist
+
+# temp
+
+(temp_hist_dog <- ggplot(doganella10, aes(temp.C))+
+    geom_histogram()+
+    theme_classic()+
+    facet_wrap(vars(temp_sensor)))
+# pretty similar distribution
+
+# rain
+(rain_hist_dog <- ggplot(doganella10, aes(rain.mm))+
+    geom_histogram()+
+    theme_classic()+
+    facet_wrap(vars(rain_sensor)))
+  # pretty similar here as well
+
+
+## vis boxplot
+
+# temp 
+(temp_box_dog <- ggplot(doganella10, aes(y=temp.C, color = temp_sensor))+
+    geom_boxplot()+
+    theme_classic())
+  # no outliers 
+
+# rain 
+(rain_box_dog <- ggplot(doganella10, aes(y = rain.mm, color = rain_sensor))+
+    geom_boxplot()+
+    theme_classic())
+   # like in histogram, there's a long tail, which the plot recognises as outliers 
+
+
+### let's check it statistically 
+
+out_tempv_dog <- boxplot.stats(doganella9$Temperature_Velletri)
+out_tempv_dog
+  # confirmed no outliers for temp velletri 
+
+out_tempm_dog <- boxplot.stats(doganella9$Temperature_Monteporzio)
+out_tempm_dog
+  # no outliers for temp monteporzio
+
+out_rainv_dog <- boxplot.stats(doganella9$Rainfall_Velletri)$out
+out_rainv_dog
+  # more than 900 outliers... 
+
+out_rainm_dog <- boxplot.stats(doganella9$Rainfall_Monteporzio)$out
+out_rainm_dog
+  # same here 
+
+## let's check where these "outliers" occur in time (rain)
+
+  # monteporzio
+out_rainm_dog_ind <- which(doganella9$Rainfall_Monteporzio %in% c(out_rainm_dog))
+out_rainm_dog_ind
+
+df_rainm_out <- doganella9[out_rainm_dog_ind,]
+df_rainm_out
+
+  # plotting it over time 
+(plot_rainm_out <- ggplot(df_rainm_out, aes(Date, Rainfall_Monteporzio))+
+    geom_point()+
+    geom_line(data = doganella9, aes(Date, Rainfall_Monteporzio, color = "red"))+
+    theme_classic())
+  # it occurs throughout - from 2012 through to 2020
+
+
+  # velletri 
+out_rainv_dog_ind <- which(doganella9$Rainfall_Velletri %in% c(out_rainv_dog))
+out_rainv_dog_ind
+length(out_rainv_dog_ind)
+
+df_rainv_dog <- doganella9[out_rainv_dog_ind,]
+View(df_rainv_dog)
+
+  # plotting over time 
+(plot_rainv_dog <- ggplot(doganella9, aes(Date, Rainfall_Velletri))+
+    geom_line(color = "red")+
+    geom_point(data = df_rainv_dog, aes(Date, Rainfall_Velletri))+
+    theme_classic())
+
+(plot_rainv_dog <- ggplot(df_rainv_dog, aes(Date, Rainfall_Velletri))+
+    geom_point()+
+    theme_classic())
+
+#### decision to leave them as such 
+
+
+#### checking out for missing - rain and temp ####
+
+# rain
+statsNA(doganella9$Rainfall_Monteporzio)
+
+ggplot_na_distribution(doganella9$Rainfall_Monteporzio)
+
+statsNA(doganella9$Rainfall_Velletri)
+
+ggplot_na_distribution(doganalla9$Rainfall_Velletri)
+
+(rain_dog <- ggplot(doganella10, aes(Date, rain.mm, color = rain_sensor))+
+    geom_line()+
+    theme_classic())
+
+# temp 
+statsNA(doganella9$Temperature_Monteporzio)
+
+ggplot_na_distribution(doganella9$Temperature_Monteporzio)
+
+statsNA(doganella9$Temperature_Velletri)
+
+ggplot_na_distribution(doganella9$Temperature_Velletri)
+
+#### filling in missing data - rain and temp - with na_ma (imputeTS) ####
+
+doganella11 <- doganella9 %>% 
+  mutate(imp_rain_velletri = na_ma(Rainfall_Velletri, k = 1),
+         imp_rain_monteporzio = na_ma(Rainfall_Monteporzio, k = 1),
+         imp_temp_velletri = na_ma(Temperature_Velletri, k = 1, maxgap = 40),
+         imp_temp_monteporzio = na_ma(Temperature_Monteporzio, k = 1, maxgap = 40))
+
+summary(doganella11$imp_rain_monteporzio)
+summary(doganella11$imp_rain_velletri)
+summary(doganella11$imp_temp_monteporzio) # longer gaps than 40 rows still na
+summary(doganella11$imp_temp_velletri) # longer gaps than 40 rows still na
+
+#### need to look for meteo data to fill in these gaps!!!
+#########################################################
+
+## vis distrib data with imputed vars 
+
+doganella12 <- doganella11 %>% 
+  select(Date, imp_rain_velletri, imp_rain_monteporzio, 
+         imp_temp_monteporzio, imp_temp_velletri) %>% 
+  gather(key = "temp", value = "temp.C", imp_temp_monteporzio, imp_temp_velletri) %>% 
+  gather(key = "rain", value = "rain.mm", imp_rain_velletri, imp_rain_monteporzio)
+
+(imp_temp_dog <- ggplot(doganella12, aes(Date, temp.C, color = temp))+
+    geom_line()+
+    theme_classic())# major gaps left 
+
+  # comparing to dataset before imputation
+(temp_dog <- ggplot(doganella10, aes(Date, temp.C, color = temp_sensor))+
+    geom_line()+
+    theme_classic())
+
+
+(imp_rain_dog <- ggplot(doganella12, aes(Date, rain.mm, color = rain))+
+    geom_line()+
+    theme_classic())
+
+  # comparing to dataset before imputation
+(rain_dog <- ggplot(doganella10, aes(Date, rain.mm, color = rain_sensor))+
+    geom_line()+
+    theme_classic())
+
+
+#### looking at last feature: volume ####
+
+# volume is of the water pumped OUT from the drinking water plant 
+# thus removed from the aquifer
+
+doganella_vol <- doganella %>% 
+  select(Date, Volume_Pozzo_1: Volume_Pozzo_9)  %>%
+  gather(key = "pozzo", value = "volume.mc", -Date) %>%
+  mutate(Date = ymd(Date))
+str(doganella_vol)
+
+(vol_dog <- ggplot(doganella_vol, aes(Date, volume.mc, color = pozzo,
+                                      group = pozzo))+
+    geom_line()+
+    theme_classic()+
+    scale_x_date(date_breaks = "1 year", date_labels = "%Y"))
+
+# boxplots 
+
+(vol_box_dog <- ggplot(doganella_vol, aes(y = volume.mc, color = pozzo))+
+    geom_boxplot()+
+    theme_classic())
+
+# hist 
+(vol_hist_dog <- ggplot(doganella_vol, aes(volume.mc))+
+    geom_histogram()+
+    facet_wrap(vars(pozzo))+
+    theme_classic())
+
+## to use these data:
+# need to remove the first years where there's total absence
+# ohterwise how to predict it? impossible 
+## or try and find data on it - but think it'll be pretty much impossible since there's no chance 
+# if acea doesn't have it who would? 
+## if i remove the first years i would be left with a very small dataset indeed
+## might have to consider leaving these values aside and not use them for the model...? 
+## otherwise i can't model just from the last few years, 
+# if the target variable, of interest, is present from the years before 
+## it would then be wiser to not consider volume data, and keep years where target data is available 
+# and just use rain and temp as features 
+#######################################################################################################
+
+
+
+
+
+
+
+
+
+
+
 
 
