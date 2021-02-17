8eef88d0098f7655b37c3bb77d2f6f63cd831031
Anna Chirumbolo <anna.chirumbolo@gmail.com>
2021-02-17 17:34:52 +0100

    added gbm model analyses for all datasets, to choose for best one --> ended up being canneto original (no changes to min rainfall levels)

--- scripts/07.R
+++ scripts/07.R
@@ -26,7 +26,7 @@ library(purrr)
 #### uploading all the files ####
 
 canneto_orig <- read.csv("processed_data/MADONNA_DI_CANNETO_to_model+lags.csv") %>%
-  dplyr::select(-X, -Date, -Fl_rate.Tri, -Fl_rate.Quar, 
+  dplyr::select(-X, -Date,-Fl_rate.Tri, -Fl_rate.Quar, 
                 -Fl_rate.Sem,-Y_m, -Year,-Month,-Day,
                 -Trimonthly,
                 -Semester, -Quarters)
@@ -36,36 +36,31 @@ str(canneto_orig)
 #str(canneto_orig)
 
 canneto0.5 <- read.csv("processed_data/canneto_rain0.5.csv")%>%
-  dplyr::select(-X, -Date, -Fl_rate.Tri, -Fl_rate.Quar, 
+  dplyr::select(-X, -Date,-Fl_rate.Tri, -Fl_rate.Quar, 
                 -Fl_rate.Sem,-Y_m, -Year,-Month,-Day,
-                -seq.rain.val,-Semester,-Quarters,-Trimonthly,
+                -Trimonthly,
+                -Semester, -Quarters,
                 -Rainfall_Settefrati)
 str(canneto0.5)
+
 canneto1.5 <- read.csv("processed_data/canneto_rain1.5.csv") %>%
   dplyr::select(-X, -Date, -Fl_rate.Tri, -Fl_rate.Quar, 
                 -Fl_rate.Sem,-Y_m, -Year,-Month,-Day,
-                -seq.rain.val,-Semester,-Quarters,
                 -Trimonthly,
+                -Semester, -Quarters,
                 -Rainfall_Settefrati)
 
 canneto3 <- read.csv("processed_data/canneto_rain3.csv") %>% 
-  dplyr::select(-X, -Date, -Fl_rate.Tri, -Fl_rate.Quar, 
+  dplyr::select(-X, -Date,-Fl_rate.Tri, -Fl_rate.Quar, 
                 -Fl_rate.Sem,-Y_m, -Year,-Month,-Day,
-                -seq.rain.val,-Semester,-Trimonthly,
-                -Quarters,-Rainfall_Settefrati)
+                -Trimonthly,
+                -Semester, -Quarters,-Rainfall_Settefrati)
 
 canneto5 <- read.csv("processed_data/canneto_rain5.csv") %>% 
-  dplyr::select(-X, -Date, -Fl_rate.Tri, -Fl_rate.Quar, 
+  dplyr::select(-X, -Date,-Fl_rate.Tri, -Fl_rate.Quar, 
                 -Fl_rate.Sem,-Y_m, -Year,-Month,-Day,
-                -seq.rain.val,-Semester,-Trimonthly,
-                -Quarters,-Rainfall_Settefrati)
-
-canneto7 <- read.csv("processed_data/canneto_rain7.csv") %>% 
-  dplyr::select(-X, -Date, -Fl_rate.Tri, -Fl_rate.Quar, 
-                -Fl_rate.Sem,-Y_m, -Year,-Month,-Day,
-                -seq.rain.val,-Semester,-Quarters,
                 -Trimonthly,
-                -Rainfall_Settefrati)
+                -Semester, -Quarters,-Rainfall_Settefrati)
 
 #### starting with stepwise model selection (regr.) ####
 
@@ -87,38 +82,32 @@ step.wisef <- function(x, DATA){
 # temp removing trimonthly data 
 canneto_orig.sw <- step.wisef("fl_rate.Ls", canneto_orig)
 canneto_orig.sw$bestTune
-coef(canneto_orig.sw$finalModel,3)
+coef(canneto_orig.sw$finalModel,6)
 
 ## canneto 0.5 mm
 
 canneto0.5.sw <- step.wisef("fl_rate.Ls",canneto0.5)
 canneto0.5.sw$bestTune
-coef(canneto0.5.sw$finalModel, 4)
+coef(canneto0.5.sw$finalModel, 6)
 
 ## canneto 1.5 mm
 
 canneto1.5.sw <- step.wisef("fl_rate.Ls",canneto1.5)
 canneto1.5.sw$bestTune
-coef(canneto1.5.sw$finalModel,3) # rain is absent (will still put it in the model for comparison)
+coef(canneto1.5.sw$finalModel,5) # rain is absent (will still put it in the model for comparison)
 
 
 ## canneto 3 mm
 
 canneto3.sw <- step.wisef("fl_rate.Ls",canneto3)
 canneto3.sw$bestTune
-coef(canneto3.sw$finalModel,3)
+coef(canneto3.sw$finalModel,11)
 
 ## canneto 5mm 
 
 canneto5.sw <- step.wisef("fl_rate.Ls",canneto5)
 canneto5.sw$bestTune
-coef(canneto5.sw$finalModel,3)
-
-## canneto 7 mm
-
-canneto7.sw <- step.wisef("fl_rate.Ls",canneto7)
-canneto7.sw$bestTune
-coef(canneto7.sw$finalModel,3)
+coef(canneto5.sw$finalModel,8)
 
 
 #### selecting variables to model ####
@@ -126,8 +115,23 @@ coef(canneto7.sw$finalModel,3)
 str(canneto_orig)
 canneto_orig_tm <- canneto_orig %>% 
   dplyr::select(Rainfall_Settefrati,Temperature_Settefrati,
+                Spring,Summer,lag1,lag7,
                 fl_rate.Ls, Autumn,snow.no)
 
+
+#### correlation matrix ####
+library(corrplot)
+
+
+canneto_orig.cor <- canneto_orig %>% 
+  cor(., use = "complete.obs") %>%
+  corrplot(., method = "circle",
+           tl.col = "black",tl.srt = 35, 
+           tl.cex = 0.72)
+
+canneto0.5.cor <- canneto0.5 %>% 
+  cor(., use = "complete.obs") %>% 
+  corrplot(., method = "circle")
 
 
 #### model: gbm ####
@@ -137,17 +141,17 @@ canneto_orig_tm <- canneto_orig %>%
 ##testing and training split
 
 set.seed(123)
-canneto_orig_tm.split <- initial_split(canneto_orig_tm, prop = .7)
+canneto_orig_tm.split <- initial_split(canneto_orig, prop = .7)
 canneto_orig_tm.train <- training(canneto_orig_tm.split)
 canneto_orig_tm.test <- testing(canneto_orig_tm.split)
 
 canneto_orig_tm.fit <- gbm::gbm(fl_rate.Ls ~ .,
-                        data = canneto_orig_tm,
+                        data = canneto_orig,
                         verbose = T, 
                         shrinkage = 0.01,
                         interaction.depth = 3, 
                         n.minobsinnode = 5,
-                        n.trees = 2000,
+                        n.trees = 1000,
                         cv.folds = 12)
 canneto_orig_tm.fit.perf <- gbm.perf(canneto_orig_tm.fit, method = "cv")
 
@@ -160,20 +164,164 @@ canneto_orig_tm.fit.rmse <- Metrics::rmse(actual = canneto_orig_tm.test$fl_rate.
                            predicted = canneto_orig_tm.fit.pred)
 print(canneto_orig_tm.fit.rmse) # 26.59
 
+
+# summarise model 
+
+p9.effects <- tibble::as_tibble(gbm::summary.gbm(canneto_orig_tm.fit,
+                                                 plotit = F))
+p9.effects %>% utils::head() # 100 ... of course it's the only one 
+# this creates new dataset with var, factor variable with variables 
+# in our model, and rel.inf - relative influence each var has on model pred 
+
+## vis distribution of predicted compared with actual target values 
+# by predicting these vals and plotting the difference 
+
+# predicted 
+
+canneto_orig_tm.test$predicted <- as.integer(predict(canneto_orig_tm.fit,
+                                        newdata = canneto_orig_tm.test,
+                                        n.trees = canneto_orig_tm.fit.perf))
+
+# plot predicted vs actual
+
+ggplot(canneto_orig_tm.test) +
+  geom_point(aes(x = predicted,
+                 y = fl_rate.Ls,
+                 color = predicted - fl_rate.Ls),
+             alpha = .7, size = 2) +
+  theme_classic()
+
+
+## plotting top 6 features
+
+p9.effects %>% 
+  arrange(desc(rel.inf)) %>% 
+  top_n(6) %>%  # it's already only 3 vars
+  ggplot(aes(x = fct_reorder(.f = var,
+                             .x = rel.inf),
+             y = rel.inf,
+             fill = rel.inf))+
+  geom_col()+
+  coord_flip()+
+  scale_color_brewer(palette = "Dark2") +
+  theme_classic()
+
+## re-doing the model with top 6 features 
+
+canneto_top6 <- canneto_orig %>% 
+  dplyr::select(fl_rate.Ls, Temperature_Settefrati, 
+                lag7,Rainfall_Settefrati,Spring,lag1,
+                Summer)
+
+set.seed(123)
+canneto_orig_tm.split <- initial_split(canneto_top6, prop = .7)
+canneto_orig_tm.train <- training(canneto_orig_tm.split)
+canneto_orig_tm.test <- testing(canneto_orig_tm.split)
+
+canneto_orig_tm.fit <- gbm::gbm(fl_rate.Ls ~ .,
+                                data = canneto_top6,
+                                verbose = T, 
+                                shrinkage = 0.01,
+                                interaction.depth = 3, 
+                                n.minobsinnode = 5,
+                                n.trees = 500,
+                                cv.folds = 12)
+canneto_orig_tm.fit.perf <- gbm.perf(canneto_orig_tm.fit, method = "cv")
+
+## make predictions 
+
+
+canneto_orig_tm.fit.pred <- stats::predict(object = canneto_orig_tm.fit,
+                                           newdata = canneto_orig_tm.test,
+                                           n.trees = canneto_orig_tm.fit.perf)
+canneto_orig_tm.fit.rmse <- Metrics::rmse(actual = canneto_orig_tm.test$fl_rate.Ls,
+                                          predicted = canneto_orig_tm.fit.pred)
+print(canneto_orig_tm.fit.rmse) # 29 still larger error with only those vals... 
+
+
+### comparing with values suggested by stepwise ###
+
+
+set.seed(123)
+canneto_orig_tm.split <- initial_split(canneto_orig_tm, prop = .7)
+canneto_orig_tm.train <- training(canneto_orig_tm.split)
+canneto_orig_tm.test <- testing(canneto_orig_tm.split)
+
+canneto_orig_tm.fit <- gbm::gbm(fl_rate.Ls ~ .,
+                                data = canneto_orig_tm,
+                                verbose = T, 
+                                shrinkage = 0.01,
+                                interaction.depth = 3, 
+                                n.minobsinnode = 5,
+                                n.trees = 500,
+                                cv.folds = 12)
+canneto_orig_tm.fit.perf <- gbm.perf(canneto_orig_tm.fit, method = "cv")
+
+## make predictions 
+
+canneto_orig_tm.fit.pred <- stats::predict(object = canneto_orig_tm.fit,
+                                           newdata = canneto_orig_tm.test,
+                                           n.trees = canneto_orig_tm.fit.perf)
+canneto_orig_tm.fit.rmse <- Metrics::rmse(actual = canneto_orig_tm.test$fl_rate.Ls,
+                                          predicted = canneto_orig_tm.fit.pred)
+print(canneto_orig_tm.fit.rmse) # 28.83
+
+
+# summarise model 
+
+p9.effects <- tibble::as_tibble(gbm::summary.gbm(canneto_orig_tm.fit,
+                                                 plotit = F))
+p9.effects %>% utils::head() # 100 ... of course it's the only one 
+# this creates new dataset with var, factor variable with variables 
+# in our model, and rel.inf - relative influence each var has on model pred 
+
+## vis distribution of predicted compared with actual target values 
+# by predicting these vals and plotting the difference 
+
+# predicted 
+
+canneto_orig_tm.test$predicted <- as.integer(predict(canneto_orig_tm.fit,
+                                                     newdata = canneto_orig_tm.test,
+                                                     n.trees = canneto_orig_tm.fit.perf))
+
+# plot predicted vs actual
+
+ggplot(canneto_orig_tm.test) +
+  geom_point(aes(x = predicted,
+                 y = fl_rate.Ls,
+                 color = predicted - fl_rate.Ls),
+             alpha = .7, size = 2) +
+  theme_classic()
+
+
+## plotting top 6 features
+
+p9.effects %>% 
+  arrange(desc(rel.inf)) %>% 
+  top_n(6) %>%  # it's already only 3 vars
+  ggplot(aes(x = fct_reorder(.f = var,
+                             .x = rel.inf),
+             y = rel.inf,
+             fill = rel.inf))+
+  geom_col()+
+  coord_flip()+
+  scale_color_brewer(palette = "Dark2") +
+  theme_classic()
+
+
+############-------------------------------------
+
+
 #### canneto at rain: 0.5 = 0 ####
 
-canneto0.5.tm <- canneto0.5 %>% 
-  dplyr::select(rain1, Temperature_Settefrati,
-                fl_rate.Ls,Spring,Summer,snow.no)
-
 ## testing and training split 
-
-canneto0.5.tm.split <- initial_split(canneto0.5.tm, prop = .7)
+str(canneto0.5)
+canneto0.5.tm.split <- initial_split(canneto0.5, prop = .7)
 canneto0.5.tm.train <- training(canneto0.5.tm.split)
 canneto0.5.tm.test <- testing(canneto0.5.tm.split)
 
 canneto0.5.tm.fit <- gbm::gbm(fl_rate.Ls ~ .,
-                                data = canneto0.5.tm,
+                                data = canneto0.5,
                                 verbose = T, 
                                 shrinkage = 0.01,
                                 interaction.depth = 3, 
@@ -189,12 +337,89 @@ canneto0.5.tm.fit.pred <- stats::predict(object = canneto0.5.tm.fit,
                                            n.trees = canneto0.5.tm.fit.perf)
 canneto0.5.tm.fit.rmse <- Metrics::rmse(actual = canneto0.5.tm.test$fl_rate.Ls,
                                           predicted = canneto0.5.tm.fit.pred)
-print(canneto0.5.tm.fit.rmse) # 26.23
+print(canneto0.5.tm.fit.rmse) # 28
 
 
 
+# summarise model 
 
+p9.effects <- tibble::as_tibble(gbm::summary.gbm(canneto_orig_tm.fit,
+                                                 plotit = F))
+p9.effects %>% utils::head() # 100 ... of course it's the only one 
+# this creates new dataset with var, factor variable with variables 
+# in our model, and rel.inf - relative influence each var has on model pred 
 
+## vis distribution of predicted compared with actual target values 
+# by predicting these vals and plotting the difference 
 
+# predicted 
 
+canneto_orig_tm.test$predicted <- as.integer(predict(canneto_orig_tm.fit,
+                                                     newdata = canneto_orig_tm.test,
+                                                     n.trees = canneto_orig_tm.fit.perf))
+
+# plot predicted vs actual
+
+ggplot(canneto_orig_tm.test) +
+  geom_point(aes(x = predicted,
+                 y = fl_rate.Ls,
+                 color = predicted - fl_rate.Ls),
+             alpha = .7, size = 2) +
+  theme_classic() ### same results as 1.5mm
+
+### canneto 3mm
+
+## testing and training split 
+
+canneto3.tm.split <- initial_split(canneto3, prop = .7)
+canneto3.tm.train <- training(canneto3.tm.split)
+canneto3.tm.test <- testing(canneto3.tm.split)
+
+canneto3.tm.fit <- gbm::gbm(fl_rate.Ls ~ .,
+                              data = canneto3,
+                              verbose = T, 
+                              shrinkage = 0.01,
+                              interaction.depth = 3, 
+                              n.minobsinnode = 5,
+                              n.trees = 5000,
+                              cv.folds = 10)
+canneto3.tm.fit.perf <- gbm.perf(canneto3.tm.fit, method = "cv")
+
+## make predictions 
+
+canneto3.tm.fit.pred <- stats::predict(object = canneto3.tm.fit,
+                                         newdata = canneto3.tm.test,
+                                         n.trees = canneto3.tm.fit.perf)
+canneto3.tm.fit.rmse <- Metrics::rmse(actual = canneto3.tm.test$fl_rate.Ls,
+                                        predicted = canneto3.tm.fit.pred)
+print(canneto3.tm.fit.rmse) # 28.55
+
+## greater error so far 
+
+### canneto 5mm 
+
+canneto5.tm.split <- initial_split(canneto5, prop = .7)
+canneto5.tm.train <- training(canneto5.tm.split)
+canneto5.tm.test <- testing(canneto5.tm.split)
+
+canneto5.tm.fit <- gbm::gbm(fl_rate.Ls ~ .,
+                            data = canneto5,
+                            verbose = T, 
+                            shrinkage = 0.01,
+                            interaction.depth = 3, 
+                            n.minobsinnode = 5,
+                            n.trees = 5000,
+                            cv.folds = 10)
+canneto5.tm.fit.perf <- gbm.perf(canneto5.tm.fit, method = "cv")
+
+## make predictions 
+
+canneto5.tm.fit.pred <- stats::predict(object = canneto5.tm.fit,
+                                       newdata = canneto5.tm.test,
+                                       n.trees = canneto5.tm.fit.perf)
+canneto5.tm.fit.rmse <- Metrics::rmse(actual = canneto5.tm.test$fl_rate.Ls,
+                                      predicted = canneto5.tm.fit.pred)
+print(canneto5.tm.fit.rmse) # 29.266
+
+## canneto_orig still best one 
 
