d7d6ac82bca5bf09b1e014f785b104723672bf7a
Anna Chirumbolo <anna.chirumbolo@gmail.com>
2021-02-23 12:20:27 +0100

    added comment about lupa water spring

---
+++ scripts/LUPA_all.R
@@ -0,0 +1,185 @@
+#### lupa ####
+
+
+#### custom functions ####
+
+## creating function to read multiple files at once 
+
+read_plus <- function(flnm) {
+  read_csv(flnm) %>% 
+    mutate(filename = flnm)
+}
+
+
+## Function to automate creation of seasons as categorical features 
+
+add.seasons <- function(data) {
+  seasons <- data %>% 
+    mutate(Date = lubridate::as_date(Date),
+           Month_day = format(Date,format = "%m-%d"),
+           
+           Spring = factor(ifelse(Month_day >= "03-21" & Month_day < "06-21",
+                                  1,0)),
+           Summer = factor(ifelse(Month_day >="06-21" & Month_day < "09-21",
+                                  1,0)),
+           Autumn = factor(ifelse(Month_day >= "09-21" & Month_day < "12-21",
+                                  1,0)),
+           Winter = factor(ifelse(Month_day >= "12-21" & Month_day <= "12-31",
+                                  1, 
+                                  ifelse(Month_day >= "01-01" & Month_day < "03-21",
+                                         1, 0)))) %>%
+    dplyr::select(-Month_day)
+  return(seasons)
+}
+
+
+## creating function for stepwise model selection
+
+step.wisef <- function(x, DATA){
+  set.seed(123)
+  train.control <- trainControl(method = "cv", number = 10)
+  step.model <- train(as.formula(paste(x,"~.")), data = DATA, 
+                      method = "leapSeq", 
+                      tuneGrid = data.frame(nvmax = 1:10), # in this particular case, max 13 features for the variable
+                      trControl = train.control,
+                      na.action = na.omit)
+  return(step.model)
+}
+
+getbestmodel.modif <- function (dates, values, freq, complete = 0, n_test = NA, graph = TRUE, 
+                                algos = list("my.prophet", "my.ets", "my.sarima", 
+                                             "my.tbats", "my.bats", "my.stlm", "my.shortterm"), 
+                                bagged = "auto", metric.error = my.rmse) 
+{
+  . <- NULL
+  freq.num <- getFrequency(freq)
+  if (is.na(n_test)) 
+    n_test <- freq.num[1]
+  if (bagged == "auto") 
+    algos <- list("my.prophet", "my.ets", "my.sarima", 
+                  "my.tbats", "my.bats", "my.stlm", 
+                  "my.shortterm")
+  df <- complete.ts(dates, values, freq, complete = 0)
+  fin <- max(df$dates[1:(length(df$dates) - n_test)])
+  df_filter <- dplyr::filter(df, dates <= fin)
+  full.TS <- prepare.ts(df$dates, df$val, freq, complete)
+  filtered.TS <- prepare.ts(df_filter$dates, df_filter$val, 
+                            freq, complete)
+  train <- my.predictions(prepedTS = filtered.TS, algos = algos, 
+                          n_pred = n_test) %>% dplyr::select(-.data$actual.value) %>% 
+    dplyr::full_join(df, by = "dates") %>% dplyr::rename(actual.value = .data$val)
+  errors <- dplyr::filter(train, .data$type == "mean") %>% 
+    dplyr::summarise_if(is.numeric, list(~metric.error(., 
+                                                       .data$actual.value))) %>% dplyr::select(-.data$actual.value)
+  best <- names(errors)[apply(errors, which.min, MARGIN = 1)] %>% 
+    paste("my", ., sep = ".")
+  ddd <- dplyr::filter(train, .data$type %in% c(NA, "mean")) %>% 
+    dplyr::select(-.data$type) %>% tidyr::gather(key = "algo", 
+                                                 value = "val", -.data$dates)
+  gg <- ggplot2::ggplot(ddd, ggplot2::aes(.data$dates, .data$val, 
+                                          color = .data$algo)) + 
+    ggplot2::geom_line(size = 1) + 
+    ggplot2::theme_minimal() +
+    ggplot2::xlab("")+
+    ggplot2::ylab("Flow Rate (L/s)")
+  if (graph == TRUE) {
+    print(gg)
+  }
+  return(list(prepedTS = full.TS, best = best, train.errors = errors, 
+              res.train = train, algos = algos, graph.train = gg))
+}
+
+
+#### libraries ####
+
+## installing packages
+#install.packages('caret', dependencies = TRUE)
+#install.packages("imputeTS")
+#install.packages("varhandle")
+#install.packages("autoTS")
+#install.packages("h2o")
+#install.packages("MLmetrics")
+#install.packages("stats")
+#install.packages("rsample")
+#install.packages("tsDyn")
+
+## Here are all the libraries used for this project 
+
+## need to divide them into section + brief comment on what they've been used for
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(ggpubr)
+library(imputeTS)
+library(zoo)
+library(data.table)
+library(lubridate)
+library(outliers)
+library(tidyselect)
+library(GGally)
+library(naniar)
+library(visdat)
+library(forecast)
+library(xts)
+library(caTools)
+library(reshape2)
+library(Hmisc)
+library(caret) 
+library(randomForest)
+library(tree)
+library(tsibble)
+library(gbm)
+library(rsample)
+library(scales)
+library(Metrics)
+library(here)
+library(MASS)
+library(leaps)
+library(corrplot)
+library(varhandle)
+library(autoTS)
+library(h2o)
+library(MLmetrics)
+library(stats)
+library(tsDyn)
+
+
+#### reading dataset ####
+
+lupa <- read.csv("data/Water_Spring_Lupa.csv")
+
+str(lupa)
+
+lupa1 <- lupa %>% 
+  rename(Date = ï..Date) %>% 
+  mutate(Date = dmy(Date))
+
+str(lupa1)
+
+lupa_missing <- lupa1 %>% 
+  miss_var_summary()
+
+print(lupa_missing)
+# only flow rate, target var has missing data, only 9% of total
+
+(vis_missing_lupa <- ggplot(lupa1, aes(Date, Flow_Rate_Lupa))+
+    geom_line(size = 1, color = "steelblue")+
+    ylab("Flow Rate (L/s)\n")+
+    xlab("")+
+    ggtitle("Distribution of target variable over time: Lupa\n")+
+    theme_classic())
+
+ggsave("img/lupa/first_look.jpg", vis_missing_lupa, 
+       dpi = 500, width = 8, height = 6)
+
+## removing missing 
+
+lupa_filtered <- lupa1 %>% 
+  filter(Date >= "2009-02-19") %>%
+  mutate(abs.flow_rate = abs(Flow_Rate_Lupa)) # changed flow to absolute value
+
+
+(vis_lupa <- ggplot(lupa_filtered, 
+                    aes(Date, abs.flow_rate))+
+    geom_line(size = 1)+
+    theme_classic())
